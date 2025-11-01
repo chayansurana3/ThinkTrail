c6ede85bb2587b5da72a2d6758f4273c55644911
Chayan <chayan.surana3@gmail.com>
2025-11-01 16:47:48 +0530

    Added startup logs

--- backend/src/main/java/com/thinktrail/blog/ThinktrailApplication.java
+++ backend/src/main/java/com/thinktrail/blog/ThinktrailApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThinktrailApplication {
 	public static void main(String[] args) {
+		System.out.println("🚀 Starting ThinkTrail backend...");
 		SpringApplication.run(ThinktrailApplication.class, args);
 	}
 }
