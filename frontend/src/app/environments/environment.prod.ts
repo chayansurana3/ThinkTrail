4a3543e99eb97d8d55964a4ce316ec19ed092849
Chayan <chayan.surana3@gmail.com>
2025-11-03 16:53:52 +0530

    changes to frontend api endpoints for prd

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environmentPRD = {
   production: true,
-  baseApiUrl: 'http://api',
+  baseApiUrl: 'http://https://thinktrail-288291511247.us-central1.run.app/api',
 };
 
 export const googleClientConfig = {
