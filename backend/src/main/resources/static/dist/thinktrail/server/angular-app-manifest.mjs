0e94454a2db9a5b1d7eb85b3801d7e8c77f6d26f
Chayan <chayan.surana3@gmail.com>
2025-11-09 20:52:26 +0530

    Added quill editor to writing and post component

--- backend/src/main/resources/static/dist/thinktrail/server/angular-app-manifest.mjs
+++ backend/src/main/resources/static/dist/thinktrail/server/angular-app-manifest.mjs
@@ -53,8 +53,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 9311, hash: '2b93a0b499d66a184e0a529d285ee53633642b6f67d27897381552d3eae474dd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1141, hash: '1dba39b115efe990733a1504a5438f08a9a0aa6c4049ff768775e801f3c66e77', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'styles-SHIGE3RF.css': {size: 37662, hash: 'CO8d2Leix6c', text: () => import('./assets-chunks/styles-SHIGE3RF_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 9311, hash: '8b33ce1d27d4eab658c613da422b89201174634d180eb5297d75ad7b542ec9d2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1141, hash: '2377998819ac678b4e184724733b31bc740c3a022dd7e61177a8f3523a6445f7', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-OZN4OKQQ.css': {size: 61844, hash: '+uPmz5dozXg', text: () => import('./assets-chunks/styles-OZN4OKQQ_css.mjs').then(m => m.default)}
   },
 };
